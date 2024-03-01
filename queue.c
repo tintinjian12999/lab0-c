a204d133cc8f684a8d4053327f9400de7f74cd99
tintinjian12999 <tintinjian@gmail.com>
2024-03-02 02:37:27 +0800

    Implement q_size Here, by simply iterate the queue by `list_for_each`. We can obtain the length of it.

--- queue.c
+++ queue.c
@@ -93,7 +93,13 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+    struct list_head *node;
+    int count = 0;
+    list_for_each (node, head)
+        count++;
+    return count;
 }
 
 /* Delete the middle node in queue */
