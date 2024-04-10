05b9a4ecd9fe3b335228a0c5e7457d5ed3eb463e
tintinjian12999 <tintinjian@gmail.com>
2024-04-10 21:17:42 +0800

    Calculate the fixed-point square root   In order to caculate UCB in MCTS. The fuctonality of sqrt using fixed-point operation is needed.
      To accomplish this goal, the well-known Newton's method is applied.
    By simply using a single "while" loop. The square roots of integers
    can be obtained with errors less than 0.3% with respect to
    their floating point counterpart which calculating by "sqrt()"
    function in "math.h".

--- fixed_point.c
+++ fixed_point.c
@@ -1,5 +1,7 @@
 #include "fixed_point.h"
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 fixed_point_t fixed_add(fixed_point_t a, fixed_point_t b)
 {
     return a + b;
@@ -22,11 +24,35 @@ fixed_point_t fixed_div(fixed_point_t a, fixed_point_t b)
     result >>= FRACTION_BITS;
     return result;
 }
+
+fixed_point_t fixed_sqrt(fixed_point_t a)
+{
+    fixed_point_t value = a;
+    fixed_point_t last = 0;
+    while (abs(value - last) > eps) {
+        last = value;
+        value = (value + fixed_div(a, value)) >> 1;
+    }
+    return value;
+}
 int main()
 {
-    int a = 2, b = 3;
-    fixed_point_t result = 0;
-    result = fixed_div(int2fix(a), int2fix(b));
-    int frac = frac2int(extract_frac(result));
-    printf("%d.%d\n", fix2int(result), frac);
+    FILE *file;
+    file = fopen("compare.txt", "w");
+    if (file == NULL) {
+        printf("無法打開檔案。\n");
+        return 1;
+    }
+    for (int i = 1; i < 201; i++) {
+        fixed_point_t result = 0;
+        result = fixed_sqrt(int2fix(i));
+        int frac = frac2int(extract_frac(result));
+        float result_float = 0;
+        float error = (float) fix2int(result) + frac / 1000.0;
+        result_float = sqrt(i);
+        error = 100 * fabs(error - result_float) / result_float;
+        fprintf(file, "%d %d.%d %.3f %f\n", i, fix2int(result), frac,
+                result_float, error);
+    }
+    fclose(file);
 }
